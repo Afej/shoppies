c4b397ed1438bb96e4faad3fdb05623d5c7fe8f0
Afej <afekurojoshua@gmail.com>
2021-01-11 18:37:57 +0100

    Major Project UI completed

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+const banner = document.querySelector("#banner");
+const bannerText = document.querySelector("#bannerText");
+const closeBannerBtn = document.querySelector("#closeBannerBtn");
+
+const form = document.querySelector("form");
+const search = document.querySelector("#search");
+const submitBtn = document.querySelector("#submitBtn");
+
+const moviesInfo = document.querySelector("#movies-info");
+const nominationInfo = document.querySelector("#nomination-info");
+
+const moviesList = document.querySelector("#movies-list");
+const nominationList = document.querySelector("#nomination-list");
+const nomineesContainer = document.querySelector(".nominees-container");
+
+let nominees = JSON.parse(localStorage.getItem("awardNominees")) || [];
+
 // const response = axios.get("http://www.omdbapi.com/", {
 //   params: {
 //     apikey: "f669b4b1",
